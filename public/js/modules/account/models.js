4cb9db60f455b95ff16b87dc5a9379412c470f9b
amarik <aandrew.marik@gmail.com>
2014-09-10 13:56:10 +0300

    Made factory('user') for  pllugFiles.account modul

--- public/js/modules/account/models.js
+++ public/js/modules/account/models.js
@@ -1,5 +1,1 @@
-var User = $resource('/api/v01/account/users/:id/:action', {userId:'@id'});
-var user = User.get({userId:123}, function() {
-  user.abc = true;
-  user.$save();
-});
+angular.module('pllugFiles.account').factory('user', ['$resource', function($resource){return $resource('/api/v01/account/users/:pk', {'id': '@id'})  }]);
